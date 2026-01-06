4689d6c559aa83ac3f6e98fad55299dc1e753f74
polino <polinovskyoleksandr1303@gmail.com>
2026-01-06 13:12:27 +0100

    reviewed

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/roll/:number', (req, res) => {
         if (isNaN(req.params.number)) {
         res.send('<h1>You should pick a number!</h1>')
     }
-    const roll = Math.floor(Math.random() * (req.params.number + 1))
+    const roll = Math.floor(Math.random() * (req.params.number))
     res.send(`<h1>You rolled a ${roll}</h1>`)
 })
 
@@ -33,7 +33,7 @@ app.get('/collectibles/:index', (req, res) => {
     if (req.params.index > 2) {
         res.send('<h1>This item in not yet in the stock!</h1>')
     }
-    res.send(`<h1>So, you want the ${collectibles[req.params.index].name}? For ${collectibles[req.params.index].price}, it can be yours!`)
+    res.send(`<h1>So, you want the ${collectibles[req.params.index].name}? For ${collectibles[req.params.index].price}, it can be yours!</h1>`)
 })
 
 //E.4
@@ -56,3 +56,28 @@ app.get('/collectibles/:index', (req, res) => {
 
 // REVIEW 
 
+app.get('/shoes', (req, res) => {
+  const minPrice = req.query['min-price'];
+  const maxPrice = req.query['max-price'];
+  const type = req.query['type'];
+
+  console.log(minPrice, maxPrice, type);
+
+  // shoes array 
+    const shoes = [
+      { name: "Birkenstocks", price: 50, type: "sandal" },
+      { name: "Air Jordans", price: 500, type: "sneaker" },
+      { name: "Air Mahomeses", price: 501, type: "sneaker" },
+      { name: "Utility Boots", price: 20, type: "boot" },
+      { name: "Velcro Sandals", price: 15, type: "sandal" },
+      { name: "Jet Boots", price: 1000, type: "boot" },
+      { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+  ];
+
+  const filteredShoes = shoes
+  .filter(shoe => !type || shoe.type === type) // this fill filter out by type
+  .filter(shoe => !minPrice || minPrice <= shoe.price) // this will filter out by minPrice
+  .filter(shoe => !maxPrice || maxPrice >= shoe.price) // this will filter out by maxPrice
+  // res send
+  res.send(filteredShoes);
+})
